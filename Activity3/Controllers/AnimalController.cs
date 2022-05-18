ecc5c02d8cf2e1a82f0a1404d162abd6740976c6
mirac <mirac@DESKTOP-DJBFHTF>
2022-05-18 18:58:06 +0800

    Add project files.

---
+++ Activity3/Controllers/AnimalController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activity3.Models;
+
+namespace Activity3.Controllers
+{
+    public class AnimalController : Controller
+    {
+        // GET: Animal
+
+        public Class1 Get()
+        {
+            Class1 a = new Class1();
+            a.Animal = "Cheetah";
+            a.Color = "Yellow, Black, White";
+            a.Size = "67 – 94 cm";
+            return a;
+        }
+    }
+}
---
+++ Activity3/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity3.Models
+{
+    public class Class1
+    {
+        public string Size;
+        public string Animal;
+        public string Color;
+    }
+}
